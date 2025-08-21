e7d2fa3ee81e45cf7e03d5c6b285c64541444356
JoãoCarreira <vitorlimacarreira@gmail.com>
2025-08-21 16:51:35 -0300

    Update and rename xadrez.c to xadrez-novato.c

---
+++ xadrez-novato.c
@@ -0,0 +1,28 @@
+// Nivel Novato
+#include <stdio.h>
+int main() {
+    // Definindo o número de casas a serem movidas
+    const int casasTorre = 5;
+    const int casasBispo = 5;
+    const int casasRainha = 8;
+    // Movimento da Torre: 5 casas para a direita
+    printf("Movimento da Torre:\n");
+    for (int i = 0; i < casasTorre; i++) {
+        printf("Direita\n");
+    }
+    // Movimento do Bispo: 5 casas na diagonal (Cima e Direita)
+    printf("\nMovimento do Bispo:\n");
+    int j = 0;
+    while (j < casasBispo) {
+        printf("Cima Direita\n");
+        j++;
+    }
+    // Movimento da Rainha: 8 casas para a esquerda
+    printf("\nMovimento da Rainha:\n");
+    int k = 0;
+    do {
+        printf("Esquerda\n");
+        k++;
+    } while (k < casasRainha);
+    return 0;
+}
